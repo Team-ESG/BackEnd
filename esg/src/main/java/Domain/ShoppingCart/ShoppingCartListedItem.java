1edd57f06d045423d54e3e70bd0043b6871c6c4f
dlwlehd <dltkdgns830@naver.com>
2023-03-19 22:07:35 +0900

    feat : ShoppingCartListedItem 도메인 생성

---
+++ esg/src/main/java/Domain/ShoppingCart/ShoppingCartListedItem.java
@@ -0,0 +1,27 @@
+package Domain.ShoppingCart;
+
+import Domain.Item.Item;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ShoppingCartListedItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "shoppingCart_id")
+    private ShoppingCart shoppingCart;
+
+    @OneToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int shoppingCartListedItemQuantity;
+    private int totalPrice;
+}
