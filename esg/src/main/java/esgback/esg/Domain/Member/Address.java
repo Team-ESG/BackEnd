65baae5d053845fe43529c25d2c962ff8c0f936a
JAEYEONsss <y718j@ajou.ac.kr>
2023-03-30 21:07:43 +0900

    builder 추가

--- esg/src/main/java/esgback/esg/Domain/Member/Address.java
+++ esg/src/main/java/esgback/esg/Domain/Member/Address.java
@@ -1,12 +1,23 @@
 package esgback.esg.Domain.Member;
 
 import jakarta.persistence.Embeddable;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Embeddable
+@NoArgsConstructor
 @Getter
 public class Address {
     private String firstAddr;
     private String secondAddr;
     private String thirdAddr;
+
+    @Builder
+    public Address(String firstAddr, String secondAddr, String thirdAddr) {
+        this.firstAddr = firstAddr;
+        this.secondAddr = secondAddr;
+        this.thirdAddr = thirdAddr;
+    }
 }
