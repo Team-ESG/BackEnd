831697bce62ffd569130224f50732c18eedac7d3
JAEYEONsss <y718j@ajou.ac.kr>
2023-03-31 15:51:00 +0900

    회원가입 추가

--- esg/src/main/java/esgback/esg/Service/Member/MemberJoinService.java
+++ esg/src/main/java/esgback/esg/Service/Member/MemberJoinService.java
@@ -1,5 +1,7 @@
 package esgback.esg.Service.Member;
 
+import esgback.esg.DTO.MemberJoinDto;
+import esgback.esg.Domain.Member.Address;
 import esgback.esg.Domain.Member.Member;
 import esgback.esg.Repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,15 +15,21 @@ public class MemberJoinService {
 
     private final MemberRepository memberRepository;
 
-    public Boolean checkIdDuplicate(String email) {
+    public Boolean checkIdDuplicate(String email) {// id 중복확인
         boolean check = memberRepository.existsByMemberId(email);
 
         return check;//true => 이미 존재, false => 등록 가능
     }
 
-    public Boolean checkNickNameDuplicate(String nickName) {
+    public Boolean checkNickNameDuplicate(String nickName) {//닉네임 중복확인
         boolean check = memberRepository.existsByNickName(nickName);
 
         return check;//true => 이미 존재, false => 등록 가능
+    }
+
+    public void joinMember(MemberJoinDto memberJoinDto) {//회원가입
+        Member member = Member.createMember(memberJoinDto);
+
+        memberRepository.save(member);
     }
 }
