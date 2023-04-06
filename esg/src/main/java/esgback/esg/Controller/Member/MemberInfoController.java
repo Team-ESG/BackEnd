156f085e515243b496b2a4a5282babc17c7a0bf5
JAEYEONsss <y718j@ajou.ac.kr>
2023-04-06 15:22:26 +0900

    회원 비밀번호 및 닉네임 수정 추가

--- esg/src/main/java/esgback/esg/Controller/Member/MemberInfoController.java
+++ esg/src/main/java/esgback/esg/Controller/Member/MemberInfoController.java
@@ -1,6 +1,7 @@
 package esgback.esg.Controller.Member;
 
 import esgback.esg.DTO.Code.PwdCodeRequestDto;
+import esgback.esg.DTO.Code.ResetDto;
 import esgback.esg.DTO.Member.MemberIdDto;
 import esgback.esg.DTO.Response;
 import esgback.esg.Service.Member.MemberInfoService;
@@ -42,18 +43,24 @@ public class MemberInfoController {
         }
     }//회원 비밀번호 초기화 가능한지 검증 => 비밀번호 분실 시, 비밀번호 재설정 자체가 따로 필요해 보임
 
-//    @PostMapping("/info/reset/pwd")
-//    public ResponseEntity<?> resetMemberPwd() {//
-//
-//    }//회원 비밀번호 재설정
-//
-//    @PostMapping("/info/reset/nickname")
-//    public ResponseEntity<?> resetNickname() {
-//
-//    }//회원 닉네임 재설정
-//
-//    @DeleteMapping("/info/out")
-//    public ResponseEntity<?> withdrawMember() {
-//
-//    }//회원 탈퇴
+    @PatchMapping("/info/reset/pwd")
+    public ResponseEntity<?> resetMemberPwd(@RequestBody ResetDto resetDto) {
+        try {
+            memberInfoService.resetPwd(resetDto);
+            return response.success("비밀번호 재설정 완료");
+        } catch (NullPointerException e) {
+            return response.fail("해당하는 계정이 없습니다.", HttpStatus.NOT_FOUND);
+        }
+    }//회원 비밀번호 재설정
+
+    @PatchMapping("/info/reset/nickname")
+    public ResponseEntity<?> resetNickname(@RequestBody ResetDto resetDto) {
+
+        try {
+            memberInfoService.resetNickname(resetDto);
+            return response.success("닉네임 재설정 완료");
+        } catch (NullPointerException e) {
+            return response.fail("해당하는 계정이 없습니다.", HttpStatus.NOT_FOUND);
+        }
+    }//회원 닉네임 재설정
 }
