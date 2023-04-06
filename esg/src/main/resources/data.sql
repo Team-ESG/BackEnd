8827fe5baba76deb14160b81f05d4283183398bb
JAEYEONsss <y718j@ajou.ac.kr>
2023-04-06 15:20:47 +0900

    dummy data 형식 변경

--- esg/src/main/resources/data.sql
+++ esg/src/main/resources/data.sql
@@ -1,15 +1,15 @@
-INSERT INTO Member (FIRST_ADDR, SECOND_ADDR, THIRD_ADDR, BIRTH_DATE, DISCOUNT_PRICE, MEMBER_ID, NAME, NICK_NAME, PASSWORD, PHONE_NUMBER)
- VALUES ('서울특별시', '강남구', '역삼동', '1990-01-01', 1000, 'member01', '홍길동', 'hong', 'password1234', '010-1234-5678');
+INSERT INTO Member (FIRST_ADDR, SECOND_ADDR, THIRD_ADDR, BIRTH_DATE, DISCOUNT_PRICE, MEMBER_ID, NAME, NICK_NAME, PASSWORD, PHONE_NUMBER, ROLE, SEX)
+ VALUES ('서울특별시', '강남구', '역삼동', '1990-01-01', 1000, 'member01', '홍길동', 'hong', '$2a$10$6F16tR6bSb2psdG.yM3H9OBANJgAUpmiXSQgmy73Gu7Pr/xo7TsRK', '01012345678', 'ROLE_MEMBER', 'MAN');
 
-INSERT INTO Member (FIRST_ADDR, SECOND_ADDR, THIRD_ADDR, BIRTH_DATE, DISCOUNT_PRICE, MEMBER_ID, NAME, NICK_NAME, PASSWORD, PHONE_NUMBER)
-VALUES ('부산광역시', '해운대구', '우동', '1985-05-10', 500, 'member02', '김철수', 'kim', 'pw9876', '010-9876-5432');
-
-INSERT INTO market (email, password, phone_number, photo_url, first_addr, second_addr, third_addr, owner_name)
-VALUES ('dltkdgns830@ajou.ac.kr', 'abc', '010-9973-2652', 'abcd.png', 'Ulsan', 'Namgu', 'asfsaf', '이상훈');
+INSERT INTO Member (FIRST_ADDR, SECOND_ADDR, THIRD_ADDR, BIRTH_DATE, DISCOUNT_PRICE, MEMBER_ID, NAME, NICK_NAME, PASSWORD, PHONE_NUMBER, ROLE, SEX)
+VALUES ('부산광역시', '해운대구', '우동', '1985-05-10', 500, 'member02', '김철수', 'kim', '$2a$10$pw0oAflM1QmcbFywxxWJ4eZSmi.6CzOcc/pJQDPjWvQPtAGUekHvy', '01098765432', 'ROLE_MEMBER', 'MAN');
 
 INSERT INTO market (email, password, phone_number, photo_url, first_addr, second_addr, third_addr, owner_name)
-VALUES ('abcde@ajou.ac.kr', 'asfsa', '010-2132-2552', 'ad.png', 'Seoul', 'Bukgu', 'sfsf', '양재연');
+VALUES ('dltkdgns830@ajou.ac.kr', 'abc', '01099732652', 'abcd.png', 'Ulsan', 'Namgu', 'asfsaf', '이상훈');
 
 INSERT INTO market (email, password, phone_number, photo_url, first_addr, second_addr, third_addr, owner_name)
-VALUES ('sfsfe3@ajou.ac.kr', 'abcdef', '010-9912-2522', 'acd.png', 'Suwon', 'paldal', 'asdad', '강동우');
+VALUES ('abcde@ajou.ac.kr', 'asfsa', '01021322552', 'ad.png', 'Seoul', 'Bukgu', 'sfsf', '양재연');
+
+INSERT INTO market (email, password, phone_number, photo_url, first_addr, second_addr, third_addr, owner_name)
+VALUES ('sfsfe3@ajou.ac.kr', 'abcdef', '01099122522', 'acd.png', 'Suwon', 'paldal', 'asdad', '강동우');
 
