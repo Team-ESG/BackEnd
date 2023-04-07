0f3590cd252d42b3ce8a0250141ebba69e30daab
JAEYEONsss <y718j@ajou.ac.kr>
2023-04-07 23:16:32 +0900

    프론트에서 로그인하고 백에서 아이디, 비번 받고 처리하는 로직 추가

--- esg/src/main/java/esgback/esg/Security/Filter/LoginFilter.java
+++ esg/src/main/java/esgback/esg/Security/Filter/LoginFilter.java
@@ -1,13 +1,18 @@
 package esgback.esg.Security.Filter;
 
+import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
 
 public class LoginFilter extends AbstractAuthenticationProcessingFilter {
 
@@ -17,6 +22,40 @@ public class LoginFilter extends AbstractAuthenticationProcessingFilter {
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+
+        if (request.getMethod().equalsIgnoreCase("GET")) {
+            System.out.println("GET Method not support");
+        }
+
+        Map<String, String> jsonData = parseRequestJson(request);
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                jsonData.get("id"),
+                jsonData.get("pwd")
+        );
+
+        return getAuthenticationManager().authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    /**
+     *
+     * data 보낼 때, {"id", "pwd" 로 보내야 됨}
+     */
+
+    private Map<String, String> parseRequestJson(HttpServletRequest request) {
+
+        try (Reader reader = new InputStreamReader(request.getInputStream())) {
+
+            Gson gson = new Gson();
+
+            return gson.fromJson(reader, Map.class);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         return null;
     }
+    /**
+     * 토큰 만들려면 POST 방식으로 id, pwd 보내야 됨
+     */
 }
