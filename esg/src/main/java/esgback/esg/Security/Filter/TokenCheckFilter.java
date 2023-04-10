57cf5a39ca059b3943fc12d09fbde73dd6db6b2a
JAEYEONsss <y718j@ajou.ac.kr>
2023-04-10 16:06:24 +0900

    accessToken 검증 추가

---
+++ esg/src/main/java/esgback/esg/Security/Filter/TokenCheckFilter.java
@@ -0,0 +1,70 @@
+package esgback.esg.Security.Filter;
+
+import esgback.esg.Exception.AccessTokenException;
+import esgback.esg.Util.JWTUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class TokenCheckFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+
+        if (!requestURI.startsWith("/api/")) {//request uri가 api로 시작하지 않을 때, 추후에 변경
+            filterChain.doFilter(request, response);
+            return;
+        }
+        System.out.println("in");
+        try {
+            Map<String, Object> value = validAccessToken(request);
+            filterChain.doFilter(request, response);
+        } catch (AccessTokenException accessTokenException) {
+            accessTokenException.sendResponseError(response);
+        }
+    }
+
+    private Map<String, Object> validAccessToken(HttpServletRequest request) throws AccessTokenException {
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null || authorization.length() < 8) {
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.UNACCEPT);
+        }
+
+        String tokenType = authorization.substring(0, 6);
+        String tokenContent = authorization.substring(7);
+
+        if (!tokenType.equalsIgnoreCase("Bearer")) {
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.BADTYPE);
+        }
+
+        try {
+            Map<String, Object> tokenValue = jwtUtil.validateToken(tokenContent);
+
+            return tokenValue;
+        } catch (MalformedJwtException malformedJwtException) {
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.MALFORM);
+        } catch (SignatureException signatureException) {
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.BADSIGN);
+        } catch (ExpiredJwtException expiredJwtException) {
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.EXPIRED);
+        }
+
+    }
+}
