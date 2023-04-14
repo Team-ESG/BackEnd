b24ba033a1b0ab07e6cb36692927f15325876d6b
dlwlehd <dltkdgns830@naver.com>
2023-04-14 23:17:34 +0900

    feat : 구매는 한 가지의 상품만 사용하므로 List 삭제

--- esg/src/main/java/esgback/esg/Domain/Reserve/Reserve.java
+++ esg/src/main/java/esgback/esg/Domain/Reserve/Reserve.java
@@ -1,6 +1,7 @@
 package esgback.esg.Domain.Reserve;
 
 import esgback.esg.Domain.Enum.State;
+import esgback.esg.Domain.Item.Item;
 import esgback.esg.Domain.Member.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -14,16 +15,17 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Reserve {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany
-    @JoinColumn(name = "reservedItem_id")
-    private List<ReservedItem> reservedItems;
+    @OneToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
 
     private Date reserveDate;
     private State isSuccess;
