3ad5001511aef65fe8fe9bd1077ab694f21a5816
dlwlehd <dltkdgns830@naver.com>
2023-05-05 14:13:53 +0900

    feat/찜 기능

--- esg/src/main/java/esgback/esg/Domain/Market/Market.java
+++ esg/src/main/java/esgback/esg/Domain/Market/Market.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Entity
