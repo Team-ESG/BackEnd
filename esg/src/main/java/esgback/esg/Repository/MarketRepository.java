908aef39350a3ff6fd99eb44e9f37ff8e6c2f156
dlwlehd <dltkdgns830@naver.com>
2023-05-08 11:08:18 +0900

    feat/검색 기능 추가

--- esg/src/main/java/esgback/esg/Repository/MarketRepository.java
+++ esg/src/main/java/esgback/esg/Repository/MarketRepository.java
@@ -3,5 +3,8 @@ package esgback.esg.Repository;
 import esgback.esg.Domain.Market.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MarketRepository extends JpaRepository<Market, Long> {
+    List<Market> findByNameContaining(String keyword);
 }
---
+++ esg/src/main/java/esgback/esg/Service/Search/SearchService.java
@@ -0,0 +1,74 @@
+package esgback.esg.Service.Search;
+
+import esgback.esg.DTO.Item.SimpleItemDto;
+import esgback.esg.DTO.Market.SimpleMarketDto;
+import esgback.esg.Domain.Item.Item;
+import esgback.esg.Domain.Market.Market;
+import esgback.esg.Repository.ItemRepository;
+import esgback.esg.Repository.MarketRepository;
+import jakarta.persistence.NoResultException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SearchService {
+    private final MarketRepository marketRepository;
+    private final ItemRepository itemRepository;
+
+    public List<SimpleMarketDto> findMarketByKeyword(String keyword) {
+        List<Market> marketList = marketRepository.findByNameContaining(keyword);
+        if (marketList.isEmpty()) throw new NoResultException("검색 결과가 없습니다.");
+        else {
+            List<SimpleMarketDto> result = marketList.stream()
+                    .map(market -> new SimpleMarketDto(market.getId(), market.getName(), market.getPhotoUrl()))
+                    .toList();
+            return result;
+        }
+    }
+
+    public List<SimpleItemDto> findItemByKeyword(String keyword) {
+        List<Item> itemList = itemRepository.findByNameContaining(keyword);
+        if (itemList.isEmpty()) throw new NoResultException("검색 결과가 없습니다.");
+        else {
+            List<SimpleItemDto> result = itemList.stream()
+                    .map(item -> new SimpleItemDto(item.getMarket().getOwnerName(), item.getName(), item.getPhotoUrl()))
+                    .toList();
+            return result;
+        }
+    }
+
+    public List<SimpleMarketDto> showRandomMarket(List<SimpleMarketDto> list) {
+        Collections.shuffle(list);
+
+        List<SimpleMarketDto> result = new ArrayList<>();
+        System.out.println(list.size());
+        if (list.size() > 2) {
+            for (int i = 0; i < 3; i++) {
+                result.add(list.get(i));
+            }
+        } else return list;
+
+        return result;
+    }
+
+    public List<SimpleItemDto> showRandomItem(List<SimpleItemDto> list) {
+        Collections.shuffle(list);
+
+        List<SimpleItemDto> result = new ArrayList<>();
+        if (list.size() > 2) {
+            for (int i = 0; i < 3; i++) {
+                result.add(list.get(i));
+            }
+        } else return list;
+
+        return result;
+    }
+
+}
