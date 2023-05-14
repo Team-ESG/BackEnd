3ba1d56988d359899bd7ef78c9d29ace52d576e2
JAEYEONsss <y718j@ajou.ac.kr>
2023-05-14 13:52:57 +0900

    생년월일, 휴대폰번호 (가운데 **** 처리), 소셜 여부 제공하도록 변경

--- esg/src/main/java/esgback/esg/DTO/Member/MemberReturnDto.java
+++ esg/src/main/java/esgback/esg/DTO/Member/MemberReturnDto.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @NoArgsConstructor
 @Data
 public class MemberReturnDto {
@@ -13,17 +15,25 @@ public class MemberReturnDto {
     private String memberId;
     private String name;
     private String nickName;
+    private String phoneNumber;
     private Address address;
     private Sex sex;
     private int discountPrice;
 
+    private String birthDate;
+
+    private Boolean social;
+
     @Builder
-    public MemberReturnDto(String memberId, String name, String nickName, Address address, Sex sex, int discountPrice) {
+    public MemberReturnDto(String memberId, String name, String nickName, String phoneNumber, Address address, String birthDate, Sex sex, int discountPrice, Boolean social) {
         this.memberId = memberId;
         this.name = name;
         this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
         this.address = address;
+        this.birthDate = birthDate;
         this.sex = sex;
         this.discountPrice = discountPrice;
+        this.social = social;
     }
 }
