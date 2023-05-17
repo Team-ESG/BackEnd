2011c8c60c8b8b9245b89e37b06a5ae31d975232
JAEYEONsss <y718j@ajou.ac.kr>
2023-05-17 18:09:14 +0900

    디비에서 이메일 찾도록 메소드 생성

--- esg/src/main/java/esgback/esg/Repository/MarketRepository.java
+++ esg/src/main/java/esgback/esg/Repository/MarketRepository.java
@@ -4,7 +4,9 @@ import esgback.esg.Domain.Market.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MarketRepository extends JpaRepository<Market, Long> {
     List<Market> findByNameContaining(String keyword);
+    Optional<Market> findByEmail(String email);
 }
