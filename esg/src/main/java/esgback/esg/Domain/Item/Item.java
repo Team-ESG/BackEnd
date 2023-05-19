13bec0ad17b2fd565f743f635ab03933108bc5c7
JAEYEONsss <y718j@ajou.ac.kr>
2023-05-19 20:45:50 +0900

    builder, 업데이트 로직 추가

--- esg/src/main/java/esgback/esg/Domain/Item/Item.java
+++ esg/src/main/java/esgback/esg/Domain/Item/Item.java
@@ -2,10 +2,7 @@ package esgback.esg.Domain.Item;
 
 import esgback.esg.Domain.Market.Market;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -33,4 +30,39 @@ public class Item {
     private int itemQuantity;
     private int reservedQuantity;
     private int wishedItemAddedCount;
+
+    @Builder
+    public Item(Long id, Market market, String name, LocalDateTime expirationDate, String photoUrl, String itemDetail, int originalPrice, int discountPrice, Date registerDate, int itemQuantity, int reservedQuantity, int wishedItemAddedCount) {
+        this.id = id;
+        this.market = market;
+        this.name = name;
+        this.expirationDate = expirationDate;
+        this.photoUrl = photoUrl;
+        this.itemDetail = itemDetail;
+        this.originalPrice = originalPrice;
+        this.discountPrice = discountPrice;
+        this.registerDate = registerDate;
+        this.itemQuantity = itemQuantity;
+        this.reservedQuantity = reservedQuantity;
+        this.wishedItemAddedCount = wishedItemAddedCount;
+    }
+
+    public static Item updateItemImg(Item item, String photoUrl) {
+        Item newItem = Item.builder()
+                .id(item.getId())
+                .market(item.getMarket())
+                .name(item.getName())
+                .expirationDate(item.getExpirationDate())
+                .photoUrl(photoUrl)
+                .itemDetail(item.getItemDetail())
+                .originalPrice(item.getOriginalPrice())
+                .discountPrice(item.getDiscountPrice())
+                .registerDate(item.getRegisterDate())
+                .itemQuantity(item.getItemQuantity())
+                .reservedQuantity(item.getReservedQuantity())
+                .wishedItemAddedCount(item.getWishedItemAddedCount())
+                .build();
+
+        return newItem;
+    }
 }
